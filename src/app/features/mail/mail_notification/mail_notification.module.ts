e4896863ecf43330bb0d875a0f882fcb94183591
Rohman Nur Haqiqi <rohmandev95@gmail.com>
2019-02-22 14:21:32 +0700

    databale mail notifikasi

--- src/app/features/mail/mail_notification/mail_notification.module.ts
+++ src/app/features/mail/mail_notification/mail_notification.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 import { Mail_notificationComponent } from './mail_notification.component';
 import { SharedModule } from '@app/shared/shared.module';
 import { RouterModule } from '@angular/router';
+import { SmartadminDatatableModule } from '@app/shared/ui/datatable/smartadmin-datatable.module';
 
 @NgModule({
   imports: [
     CommonModule,
     SharedModule,
+    SmartadminDatatableModule,
     RouterModule.forChild([
       {
         path: '',
